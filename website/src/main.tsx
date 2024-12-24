4a8e3c2ad478a218a54c04d5ab2798703a51190d
kiwonbyun <bkw9603@gmail.com>
2024-12-24 13:48:22 +0900

    0.0.6배포

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -13,7 +13,7 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <MockPilotTools forceRun>
+    <MockPilotTools>
       <App />
       <Toaster position="top-right" />
     </MockPilotTools>
